16085e15fc33d7687c74ce9ebd9bf55fbb7b58df
Norrick McGee <Norrick.mcgee@gmail.com>
2019-04-19 18:05:46 -0500

    first attempt at acquire.py created

---
+++ acquire.py
@@ -0,0 +1,127 @@
+import pandas as pd
+
+
+###################
+##
+## All of this functions are coming from a file that has been imported like so:
+##
+## ##### file = open('csv-path-here').readlins()
+## ##### for index,line in enumerate(file):
+##  ##### file[index] = line.replace('\n','')
+##
+###################
+
+def grab_foods(file):
+    foods = []
+    for line in file:
+        if line == "":
+            break
+        foods.append(line)
+    foods = foods[1:]
+    return foods
+
+
+def remove_foods(file):
+    file = file.copy()
+    while True:
+        if file.pop(0) == '':
+            break
+
+    file.pop(0) ## Removes the "Activites"
+    file.pop(0) ## Removes "Column names"
+
+    return file
+
+def grab_activities(file):
+    file = file.copy()
+    file = remove_foods(file)
+    activities = {}
+    for string in file:
+        ### This checks to see if we hit the next empty string signifying we've oved on to food logs
+        if string == "":
+            break
+        x = string.split('"')
+        activities[x[1]] = {'total_calories':x[3],
+                            'steps':x[5],
+                           'distance':x[7],
+                           'floors':x[9],
+                           'mins_sedentary':x[11],
+                           'mins_active_light':x[13],
+                           'mins_active_med':x[15],
+                           'mins_active_high':x[17],
+                           'activity_calories':x[19]
+                           }
+
+    return activities
+
+def remove_activities(file):
+    file = file.copy()
+    file = remove_foods(file)
+    while True:
+        if file.pop(0) == '':
+            break
+
+    return file
+
+def grab_food_logs(file):
+    file = file.copy()
+    file = remove_foods(file)
+    file = remove_activities(file)
+    food_logs = {}
+    while True:
+        food_log = file.pop(0).split()[2]
+        file.pop(0)
+        calories = file.pop(0).split(',')[2].replace('"','')
+        fat = file.pop(0).split(',')[2].replace('"','')
+        fiber = file.pop(0).split(',')[2].replace('"','')
+        carbs = file.pop(0).split(',')[2].replace('"','')
+        sodium = file.pop(0).split(',')[2].replace('"','')
+        protien = file.pop(0).split(',')[2].replace('"','')
+        water= file.pop(0).split(',')[2].replace('"','')
+        file.pop(0)
+
+        food_logs[food_log] ={'calories' : calories,
+                         'fat' : fat,
+                         'fiber' : fiber,
+                         'carbs': carbs,
+                         'sodium' : sodium,
+                         'protien' :protien,
+                         'water' : water}
+        if len(file) == 0:
+            return food_logs
+
+
+def foods_to_dataframe(foods):
+    food_dict = {}
+    foodz = foods.copy()[1:]
+    for i in foodz:
+        split_str = i.split('"')
+        print(split_str)
+        food_dict[split_str[1]] = {'calories':split_str[3]}
+
+    return pd.DataFrame(food_dict).T
+
+def activities_to_dataframe(activities):
+    return pd.DataFrame(activities).T
+
+
+def food_logs_to_dataframe(food_logs):
+    return pd.DataFrame(food_logs).T
+
+
+def csv_to_dataframes(csv_path=''):
+    file = open(csv_path).readlines()
+    for index,line in enumerate(file):
+        file[index] = line.replace('\n','')
+
+    food_logs = grab_food_logs(file)
+    foods = grab_foods(file)
+    activities = grab_activities(file)
+
+
+
+    foods_df = foods_to_dataframe(foods)
+    activities_df = activities_to_dataframe(activities)
+    food_logs_df = food_logs_to_dataframe(food_logs)
+
+    return [foods_df,activities_df,food_logs_df]
