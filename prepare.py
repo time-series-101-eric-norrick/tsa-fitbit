0a4e20577ed71a17802f8da72ba60656d97b98c2
Norrick McGee <Norrick.mcgee@gmail.com>
2019-04-22 07:59:49 -0500

    finished building acquire and prep

--- prepare.py
+++ prepare.py
@@ -2,11 +2,19 @@ import pandas as pd
 import acquire as aq
 
 
-def set_date_times(df1,df2,df3):
+def set_date_times(foods,activ,log):
 
     # df1,df2,df3 = aq.acquire_fitbit()
-    df1.index= pd.to_datetime(df1.index)
-    df2.index= pd.to_datetime(df2.index)
-    df3.index= pd.to_datetime(df3.index)
+    foods.index= pd.to_datetime(foods.index)
+    activ.index= pd.to_datetime(activ.index)
+    log.index= pd.to_datetime(log.index)
 
-    return [df1,df2,df3]
+    return [foods,activ,log]
+
+def merge_dfs(foods,activ,log):
+    merged = pd.merge(activ.reset_index(),log.reset_index(),how='right')
+    merged = pd.merge(merged,foods.reset_index(),how='left')
+    return merged
+
+def prepare_fitbit():
+    return merge_dfs(*set_date_times(*aq.acquire_fitbit()))
